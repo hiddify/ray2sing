a29e7ba17d9b37f443dedf8ee3a4e9559f7d83ce
Hiddify <114227601+hiddify-com@users.noreply.github.com>
2023-10-05 14:10:12 +0000

    fix: vmess configs

--- ray2sing/convert.go
+++ ray2sing/convert.go
@@ -115,10 +115,30 @@ func decodeVmess(vmessConfig string) (map[string]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	var data map[string]string
+	var data map[string]interface{}
 	json.Unmarshal(decodedData, &data)
-	// fmt.Printf("----%v---", data)
-	return data, nil
+	strdata := convertToStrings(data)
+	// fmt.Printf("----%v---", strdata)
+	return strdata, nil
+}
+
+func convertToStrings(data map[string]interface{}) map[string]string {
+	stringMap := make(map[string]string)
+	for key, value := range data {
+		switch v := value.(type) {
+		case string:
+			stringMap[key] = v
+		case float64:
+			stringMap[key] = strconv.Itoa(int(v))
+		// case map[string]interface{}:
+		// 	stringMap[key] = convertToStrings(v)
+
+		default:
+			stringMap[key] = fmt.Sprintf("%v", v)
+		}
+	}
+	return stringMap
+
 }
 
 func parseProxyURL(inputURL string, protocol string) (map[string]string, error) {
@@ -146,7 +166,7 @@ func parseHysteria2(inputURL string) (result map[string]string, err error) {
 	params := parsedURL.Query()
 	name := parsedURL.Fragment
 	if name == "" {
-		name = fmt.Sprintf("hysteria2-%v-out", time.Now().UnixNano())
+		name = fmt.Sprintf("hysteria2-%v", time.Now().UnixNano())
 	}
 	result = map[string]string{
 		"protocol": "hysteria2",
@@ -267,7 +287,8 @@ func getTransportOptions(decoded map[string]string) (*T.V2RayTransportOptions, e
 	if path == "" {
 		path = decoded["serviceName"]
 	}
-	if decoded["headerType"] == "http" && net == "tcp" {
+	// fmt.Printf("\n\nheaderType:%s, net:%s, type:%s\n\n", decoded["headerType"], net, decoded["type"])
+	if decoded["type"] == "http" && net == "tcp" {
 		net = "http"
 	}
 	switch net {
@@ -349,7 +370,7 @@ func VmessSingbox(vmessURL string) (T.Outbound, error) {
 	if err != nil {
 		return T.Outbound{}, err
 	}
-
+	// fmt.Printf("port:%v", decoded["port"])
 	port := toInt16(decoded["port"])
 	transportOptions, err := getTransportOptions(decoded)
 	if err != nil {
