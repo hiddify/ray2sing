b02d6a80e0233316c1beebeeeb1aa702055f8cb5
Sarina <iam54r1n40@proton.me>
2024-03-08 01:30:13 +0330

    fix: decode ssh pk & hk from base64

--- ray2sing/ssh.go
+++ ray2sing/ssh.go
@@ -3,6 +3,7 @@ package ray2sing
 import (
 	T "github.com/sagernet/sing-box/option"
 
+	"encoding/base64"
 	"strings"
 )
 
@@ -14,16 +15,22 @@ func SSHSingbox(sshURL string) (*T.Outbound, error) {
 	decoded := u.Params
 	prefix := "-----BEGIN OPENSSH PRIVATE KEY-----\n"
 	suffix := "\n-----END OPENSSH PRIVATE KEY-----\n"
-
-	privkeys := strings.Split(decoded["pk"], ",")
+	decodedPkBytes, err := base64.StdEncoding.DecodeString(decoded["pk"])
+	if err != nil {
+		return nil, err
+	}
+	privkeys := strings.Split(string(decodedPkBytes), ",")
 	if len(privkeys) == 1 && privkeys[0] == "" {
 		privkeys = []string{}
 	}
 	for i := 0; i < len(privkeys); i++ {
 		privkeys[i] = prefix + privkeys[i] + suffix
 	}
-
-	hostkeys := strings.Split(decoded["hk"], ",")
+	decodedHkBytes, err := base64.StdEncoding.DecodeString(decoded["hk"])
+	if err != nil {
+		return nil, err
+	}
+	hostkeys := strings.Split(string(decodedHkBytes), ",")
 
 	result := T.Outbound{
 		Type: "ssh",
