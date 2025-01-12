113455799711da1886ea3529d37274cf20a22b7e
hiddify <114227601+hiddify-com@users.noreply.github.com>
2025-01-12 13:44:07 +0330

    fix: space at the end of base64 string

--- ray2sing/common.go
+++ ray2sing/common.go
@@ -249,12 +249,15 @@ func getDialerOptions(decoded map[string]string) option.DialerOptions {
 }
 
 func decodeBase64IfNeeded(b64string string) (string, error) {
+	b64string = strings.TrimSpace(b64string)
+
 	padding := len(b64string) % 4
 	b64stringFix := b64string
 	if padding != 0 {
 		b64stringFix += string("===="[:4-padding])
 	}
 	decodedBytes, err := base64.StdEncoding.DecodeString(b64stringFix)
+
 	if err != nil {
 		return b64string, err
 	}
