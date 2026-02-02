720f3b8a1e4aa748743ab155b70928ff90848961
hiddify <114227601+hiddify-com@users.noreply.github.com>
2026-02-02 23:38:11 +0330

    fix ssh issue

--- ray2sing/ssh.go
+++ ray2sing/ssh.go
@@ -12,15 +12,22 @@ func SSHSingbox(sshURL string) (*T.Outbound, error) {
 		return nil, err
 	}
 	decoded := u.Params
-	prefix := "-----BEGIN OPENSSH PRIVATE KEY-----\n"
-	suffix := "\n-----END OPENSSH PRIVATE KEY-----\n"
+	prefix := "-----BEGIN OPENSSH PRIVATE KEY-----"
+	suffix := "-----END OPENSSH PRIVATE KEY-----"
 
 	privkeys := strings.Split(decoded["pk"], ",")
 	if len(privkeys) == 1 && privkeys[0] == "" {
 		privkeys = []string{}
 	}
 	for i := 0; i < len(privkeys); i++ {
-		privkeys[i] = prefix + privkeys[i] + suffix
+		if !strings.Contains(privkeys[i], prefix) {
+			privkeys[i] = prefix + "\n" + privkeys[i]
+		}
+		if !strings.Contains(privkeys[i], suffix) {
+			privkeys[i] = privkeys[i] + "\n" + suffix
+		}
+		privkeys[i] = strings.ReplaceAll(privkeys[i], prefix, prefix+"\n")
+		privkeys[i] = strings.ReplaceAll(privkeys[i], suffix, "\n"+suffix)
 	}
 
 	hostkeys := strings.Split(decoded["hk"], ",")
